fd72583c25f1998c5353afdc42db0f0073210a23
Cherry <sharareshaddev@gmail.com>
2024-05-08 19:17:53 +0330

    Provide the Redux Store to the  Application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </QueryClientProvider>
+  );
 }
