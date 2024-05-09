b2d0e9e7cc971c70048f087f15f91b606cf3730b
Cherry <sharareshaddev@gmail.com>
2024-05-09 20:45:20 +0330

    define data types and props

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,4 +2,11 @@ export interface Drug {
   id: number;
   name: string;
   price: number;
+  image?: string;
+}
+
+export interface DrugsPageProps {
+  drugs: { data: Drug[] };
+  currentPage: number;
+  totalPages: number;
 }
