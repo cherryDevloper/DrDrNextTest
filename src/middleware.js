1418c2603c80d96e61aea536ea36b73544b8fdbb
Cherry <sharareshaddev@gmail.com>
2024-05-09 20:38:58 +0330

    improved navigations and redirecting

---
+++ src/middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const url = req.nextUrl.clone();
+
+  // Redirect / to /drugs
+  if (url.pathname === "/") {
+    url.pathname = "/drugs";
+    return NextResponse.redirect(url);
+  }
+
+  if (url.pathname === "/medicine" && url.searchParams.get("page") === "1") {
+    url.searchParams.delete("page");
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/medicine"],
+};
