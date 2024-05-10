67e6e588571e0bb05d5b6084c8dbc03d73256595
Cherry <sharareshaddev@gmail.com>
2024-05-10 10:29:08 +0330

    removed unnecessary redirection

--- src/middleware.js
+++ src/middleware.js
@@ -9,10 +9,10 @@ export function middleware(req) {
     return NextResponse.redirect(url);
   }
   //testing something ...
-  if (url.pathname === "/medicine" && url.searchParams.get("page") === "1") {
-    url.searchParams.delete("page");
-    return NextResponse.redirect(url);
-  }
+  // if (url.pathname === "/medicine" && url.searchParams.get("page") === "1") {
+  //   url.searchParams.delete("page");
+  //   return NextResponse.redirect(url);
+  // }
 
   return NextResponse.next();
 }
