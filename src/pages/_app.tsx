2000859cceb2553e2c9ed1c7443f23997244d8fc
Cherry <sharareshaddev@gmail.com>
2024-05-17 16:29:47 +0330

    removed unused libraries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,14 @@
 import "@/styles/globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import NextNProgress from "nextjs-progressbar";
 import { store } from "@/redux/store";
-const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <NextNProgress height={8} color="#209cee" />
-        <Component {...pageProps} />
-      </Provider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <NextNProgress height={8} color="#209cee" />
+      <Component {...pageProps} />
+    </Provider>
   );
 }
