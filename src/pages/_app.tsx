9d25014ff034e6de004b2e272fa09e64432e2b87
Cherry <sharareshaddev@gmail.com>
2024-05-18 08:04:23 +0330

    removed unused packages and libraries such as redux and react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
 import NextNProgress from "nextjs-progressbar";
-import { store } from "@/redux/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
+    <>
       <NextNProgress height={8} color="#209cee" />
       <Component {...pageProps} />
-    </Provider>
+    </>
   );
 }
